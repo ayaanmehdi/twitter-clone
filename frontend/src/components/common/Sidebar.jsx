819e1096f0eec3ff27e9d21f8b08a391382552eb
ayaandmr <mehdiayaan786@gmail.com>
2024-11-21 13:01:45 +0530

    was having a truble

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -75,7 +75,7 @@ const Sidebar = () => {
 						</Link>
 					</li>
 				</ul>
-				{data && (
+				{authUser && (
 					<Link
 						to={`/profile/${authUser.username}`}
 						className='mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full'
